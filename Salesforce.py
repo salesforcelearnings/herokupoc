af3a07f2ca2fbd3dbf7715678f08341d427a4420
salesforcelearnings <142854489+salesforcelearnings@users.noreply.github.com>
2023-09-20 05:28:46 -0700

    Sixth Commit

--- Salesforce.py
+++ Salesforce.py
@@ -10,7 +10,7 @@ PASSWORD = os.getenv('PASSWORD');
 SECURITY_TOKEN = os.getenv('SECURITY_TOKEN');
 
 def main():
-    # Authentication settings details
+    # Authentication settings details33
     sf = Salesforce(username=SALESFORCE_USERNAME,
                     password=PASSWORD,
                     security_token=SECURITY_TOKEN)
